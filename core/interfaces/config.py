325ea4d854e789abe74c05a9ca0e27ae0d178512
Ramraj S <rasilamb@microsoft.com>
2022-01-19 22:33:14 +0530

    Include class_map in config

--- core/interfaces/config.py
+++ core/interfaces/config.py
@@ -3,6 +3,7 @@
 
 from abc import ABC, abstractmethod
 
+from core.utils.helper import is_empty
 from core.utils.map import resources
 
 
@@ -11,6 +12,9 @@ class Config(ABC):
     Abstract class for the config which is one of the inputs to the Concurrent Executor
     """
     __logger = resources.get('LOGGER')
+
+    def __init__(self):
+        self.__class_map = None
 
     @abstractmethod
     def get_tasks(self):
@@ -28,9 +32,23 @@ class Config(ABC):
     def get_notifiers(self):
         raise NotImplementedError()
 
-    @abstractmethod
-    def get_instances(self):
-        raise NotImplementedError()
+    def get_instances(self, names: list):
+        """
+        Returns a list of instances identifies by the given names
+        """
+        if is_empty(self.__class_map):
+            raise
+
+        instances = []
+        for name in names:
+            inst = self.__class_map.get(name)
+            if inst is None:
+                err = 'module {} not found in class_map! Check spelling! ' \
+                      'If new entity, please add an entry in the class_map'.format(name)
+                raise ModuleNotFoundError(err)
+            instances.append(inst)
+
+        return instances
 
     def logger(self):
         return self.__logger
@@ -42,6 +60,13 @@ class ConfigBuilder(ABC):
     """
     _name = 'ConfigBuilder'
     __logger = resources.get('LOGGER')
+
+    def __init__(self):
+        self.__class_map = None
+
+    def with_class_map(self, class_map):
+        self.__class_map = class_map
+        return self
 
     @abstractmethod
     def with_tasks(self, tasks):
