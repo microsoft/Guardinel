80b7089e6b8f6cdb9501f91c5b7ac56b5157b792
Ramraj S <rasilamb@microsoft.com>
2022-01-20 12:08:47 +0530

    Add exceptions and constants files for api

---
+++ api_client/api_config_constants.py
@@ -0,0 +1,11 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+class APIConfigConstants:
+    """
+    Holds constants used commonly across all cloud api services
+    """
+
+    REPO_API_CLIENT = 'REPO_API_CLIENT'
+    WORK_ITEM_API_CLIENT = "WORK_ITEM_API_CLIENT"
+    PULL_REQUEST_API_CLIENT = "PULL_REQUEST_API_CLIENT"
