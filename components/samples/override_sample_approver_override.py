77fba180722b760e959bea0f5bbb3c34dceb2865
Ramraj S <rasilamb@microsoft.com>
2022-01-22 00:21:17 +0530

    Add approval override for sample

---
+++ components/samples/override_sample_approver_override.py
@@ -0,0 +1,9 @@
+from core.interfaces.override import Override
+
+
+class ApprovalOverride(Override):
+    def evaluate(self, input_entity):
+        if input_entity
+
+    def name(self):
+        return 'approval_override'
--- components/samples/policy_sample_hello_world.py
+++ components/samples/policy_sample_hello_world.py
@@ -3,7 +3,11 @@ from core.utils.constants import Constants
 
 
 class HelloWorldPolicy(Task):
+
     def execute(self, input_entity):
+        if input_entity.project is 'TestProject':
+            return self.result(Constants.FAIL, message="Can't execute the policy on TestProject")
+
         return self.result(Constants.SUCCESS)
 
     def overrides(self):
