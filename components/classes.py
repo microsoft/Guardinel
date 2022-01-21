16af1a78d43d94e9ca2f7b9da325b733a95406e7
Ramraj S <rasilamb@microsoft.com>
2022-01-22 00:37:40 +0530

    Add sample notifier using cmdline notifier

--- components/classes.py
+++ components/classes.py
@@ -1,6 +1,6 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
-
+from components.samples.notifier_sample_sysout_notifier import CommandLineNotifier
 from components.samples.override_sample_approver_override import ApprovalOverride
 from components.samples.policy_sample_hello_world import HelloWorldPolicy
 
@@ -9,6 +9,9 @@ instances_map = {
     'hello_world_policy': HelloWorldPolicy(),
 
     # overrides
-    'approval_override': ApprovalOverride()
+    'approval_override': ApprovalOverride(),
+
+    # notifiers
+    'cmdline_notifier': CommandLineNotifier()
 
 }
